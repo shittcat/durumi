417d409f96e7ee357038eab1e2899c6c903ec5b1
synod2 <synod2@naver.com>
2020-07-30 07:30:45 +0000

    0730_desktop

--- workspace/volumes/durumi/durumiApp/apicodes/keyword.py
+++ workspace/volumes/durumi/durumiApp/apicodes/keyword.py
@@ -1,7 +1,7 @@
 #-*- coding:utf-8 -*-
 from urllib.parse import urlencode, quote_plus
 from urllib.request import urlopen , Request
-from .apikey import * #if using on django , should using .apikey instead apikey 
+from apikey import * #if using on django , should using .apikey instead apikey 
 import json 
 
 def keywordFindAPI(inputKeyword):
@@ -9,7 +9,7 @@ def keywordFindAPI(inputKeyword):
     #find place by keyword API
     queryParams = '?' + 'ServiceKey='+ServiceKey+'&'+urlencode({ 
         quote_plus('MobileApp') : 'AppTest', 
-        quote_plus('MobileOS') : 'ETC', quote_plus('pageNo') : '1', quote_plus('numOfRows') : '10', 
+        quote_plus('MobileOS') : 'ETC', quote_plus('pageNo') : '1', quote_plus('numOfRows') : '100', 
         quote_plus('listYN') : 'Y', quote_plus('arrange') : 'A', quote_plus('contentTypeId') : '12', 
         quote_plus('areaCode') : '', quote_plus('sigunguCode') : '', quote_plus('cat1') : '', 
         quote_plus('cat2') : '', quote_plus('cat3') : '', quote_plus('keyword') : inputKeyword 
@@ -22,11 +22,19 @@ def keywordFindAPI(inputKeyword):
     getJson = json.loads(response_body)["response"]["body"]
     retItems = {}
     i=0
-    for item in getJson["items"]["item"] :
-        item = dict( item.items() )
+    
+    tcount = (getJson['totalCount'])
+    tnum = (getJson['numOfRows'])
+    
+    if (tcount == 1) or (tnum == 1) : #if result count is 1
+        item = getJson["items"]["item"]
         retItems['item'+str(i)] = ( json.dumps(item,ensure_ascii=False) )
-        i += 1
-        #make dict_items type Objects to list type objects
+    else :
+        for item in getJson["items"]["item"] :
+            item = dict( item.items() )
+            retItems['item'+str(i)] = ( json.dumps(item,ensure_ascii=False) )
+            i += 1
+            #make dict_items type Objects to list type objects
     return retItems #return datas to json data
 
 if __name__ == "__main__" : 
