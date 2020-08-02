2574cf853646c795e9dc47b15ac2eece12a3dcbc
eight <whdgus9509@gmail.com>
2020-08-02 06:41:02 +0000

    MySQL (Socket Error)

--- workspace/volumes/durumi/durumi/settings.py
+++ workspace/volumes/durumi/durumi/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'durumi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'durumiDB',
+        'USER': 'admin',
+        'PASSWORD': 'DAudrmuimni',
+        'PORT': '3306',
     }
 }
 
