ce41de5232c82fd4f75e920bab4f3772afb5bfb3
eight <whdgus9509@gmail.com>
2020-11-18 18:03:11 +0000

    Achv View Sample

--- workspace/volumes/durumi/durumiApp/admin.py
+++ workspace/volumes/durumi/durumiApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .Models.UserModel import Notice, Question
+from .Models.UserModel import Notice, Question, AchieveInfo
 
 admin.site.register(Notice)
 admin.site.register(Question)
+admin.site.register(AchieveInfo)
 # Register your models here.
