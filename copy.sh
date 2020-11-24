0804bac06d0dda3d558572ee378990010a9592e4
synod2 <synod2@naver.com>
2020-11-25 03:10:56 +0900

    change

---
+++ copy.sh
@@ -0,0 +1,5 @@
+cp ~/durumi_back/workspace/docker/.env ~/durumi/workspace/docker/.env
+cp -r ~/durumi_back/workspace/volumes/certbot ~/durumi/workspace/volumes/certbot
+cp ~/durumi_back/workspace/volumes/durumi/durumiApp/apicodes/apikey.py ~/durumi/workspace/volumes/durumi/durumiApp/apicodes/apikey.py
+cp ~/durumi_back/workspace/volumes/durumi/hides.py ~/durumi/workspace/volumes/durumi/hides.py 
+cp ~/durumi_back/workspace/volumes/durumi/durumiApp/Views/appkey.py ~/durumi/workspace/volumes/durumi/durumiApp/Views/appkey.py
